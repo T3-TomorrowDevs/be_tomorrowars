ba9b53a6aa8723e2c5a4af884d03df9aaf548e30
isabel-lombardi <isabel.lombardi96@gmail.com>
2021-07-25 11:31:01 +0200

    [FEAT] create planet-army table in models.py close #28

--- game/models.py
+++ game/models.py
@@ -3,10 +3,29 @@ from django.contrib.auth.models import User
 
 
 class GameAccount(models.Model):
-    # will hold a One-To-One relationship with the existing User Model
-    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    # If blank=True, the field is allowed to be blank
+    # if null=True, Django will store empty values as NULL in the database
+
+    # ForeignKey to define a many-to-one relationship
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     credit = models.IntegerField(default=10000)
     level = models.IntegerField(default=1, blank=True)
 
     def __str__(self):
         return str(self.user)
+
+
+
+class PlanetArmy(models.Model):
+    # If blank=True, the field is allowed to be blank
+    # if null=True, Django will store empty values as NULL in the database
+
+    # ForeignKey to define a many-to-one relationship
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+
+    planet_name = models.CharField(max_length=20)
+    army_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return str(self.user)
+
