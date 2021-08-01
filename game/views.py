7a8ce2e5d20fa7a0ef5cc142942bdff22e1d5aed
isabel-lombardi <isabel.lombardi96@gmail.com>
2021-08-01 21:36:02 +0200

    [FEAT] validate army and planet name close #79

--- game/views.py
+++ game/views.py
@@ -1,7 +1,6 @@
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.parsers import MultiPartParser
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
