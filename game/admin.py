73389ebc97b0fe03b4dbfb0a3062775eab00c361
isabel-lombardi <isabel.lombardi96@gmail.com>
2021-08-01 21:38:23 +0200

    [FEAT] UserTroop model in Admin close #83

--- game/admin.py
+++ game/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from game.models import PlanetArmy, GameAccount
+from game.models import PlanetArmy, GameAccount, UserTroop
 
 admin.site.register(PlanetArmy)
 admin.site.register(GameAccount)
+admin.site.register(UserTroop)
 
 # Register your models here.
