172c0cfd9b4facc045dbf7ce67e222f70fda710d
Francesco <cicciodb@hotmail.it>
2021-08-07 00:40:29 +0200

    created UserAccount class to store the google_id

--- user_account/models.py
+++ user_account/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserAccount(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    google_id = models.CharField(max_length=100)
--- user_account/views.py
+++ user_account/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework import status  # for send status
+from rest_framework.views import APIView  # class view
+from user_account.google_account.handle_google_data import GoogleData
+from .models import UserAccount
+from django.contrib.auth.models import User
+from random import randrange
+
+
+class LoginAPIView(APIView):
+
+    def post(self, request):
+        req_data = request.data
+
+        user_info = GoogleData.get_userinfo(req_data['access-token'])
+
+        username = "{}{}".format(user_info["name"], randrange(1, 1000))
+        first_name = user_info["given_name"]
+        last_name = user_info["family_name"]
+
+        new_account = UserAccount()
+        new_account.user = User.objects.create_user(username, first_name=first_name,
+                                            last_name=last_name)
+
+        new_account.google_id = user_info["id"]
+        new_account.save()
+
+        return Response('Correctly registered', status=status.HTTP_201_CREATED)
+
+
