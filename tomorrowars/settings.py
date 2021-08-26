8119a544e11d1f924a770ae695415177aef4991d
Isabel <72871855+isabel-lombardi@users.noreply.github.com>
2021-08-26 22:45:11 +0200

    [FIX] import dj database

--- tomorrowars/settings.py
+++ tomorrowars/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 import django_heroku
+import dj_database_url
+
 from corsheaders.defaults import default_methods, default_headers
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
